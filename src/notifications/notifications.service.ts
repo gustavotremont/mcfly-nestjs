dd8e24272350a9de669b5cc6afb715105ea2d6a6
Gustavo Tremont <gustavotremontsr@gmail.com>
2022-03-06 23:02:37 +0100

    create notifications service

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose'
+import { Notification } from 'src/models/notification.schema';
 
 @Injectable()
-export class NotificationsService {}
+export class NotificationsService {
+
+    constructor( @InjectModel(Notification.name) private readonly notificationModel: Model<Notification> ) {}
+
+    public async create(): Promise<Types.ObjectId> {
+        const newNotification = await new this.notificationModel().save()
+        return newNotification._id
+    }
+
+    public async addNewNotification( id: string, newMessage: string ): Promise<void> {
+        await this.notificationModel.findByIdAndUpdate(id, { $push: {messages: newMessage} } )
+    }
+}
