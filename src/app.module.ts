89aadec257e4b659b93974198383245db5d5457a
Gustavo Tremont <gustavotremontsr@gmail.com>
2022-03-06 21:27:32 +0100

    import modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.BBDD_URI),
-    UsersModule
+    UsersModule,
+    NotificationsModule,
+    MessagesModule
   ],
   controllers: [AppController],
   providers: [AppService],
