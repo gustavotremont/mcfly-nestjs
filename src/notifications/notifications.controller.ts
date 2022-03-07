687d664389ac9e9f2dd2dc750fd127e502a3b4f8
Gustavo Tremont <gustavotremontsr@gmail.com>
2022-03-07 03:23:30 +0100

    create notification controllers

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, NotFoundException, Param, Res } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('notifications')
-export class NotificationsController {}
+export class NotificationsController {
+    constructor(
+        private usersService: UsersService
+    ) {}
+
+    
+    @Get('/:email')
+    public async getUserByEmail(
+        @Res() res,
+        @Param('email') email: string
+    ) {
+        const user = await this.usersService.findByEmail(email)
+        if (!user) {
+            throw new NotFoundException('User does not exist!');
+        }
+        return res.status(HttpStatus.OK).json(user.notifications);
+    }
+}
