10d3b4935762f164bc47fb57db25195a00a79491
Gustavo Tremont <gustavotremontsr@gmail.com>
2022-03-07 03:21:44 +0100

    add forwardRef to import modules

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,14 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NotificationSchema, Notification } from 'src/models/notification.schema';
+import { UsersModule } from 'src/users/users.module';
+
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Notification.name, schema: NotificationSchema },
-    ])
+    ]),
+    forwardRef(() => UsersModule)
   ],
   providers: [NotificationsService],
   controllers: [NotificationsController],
